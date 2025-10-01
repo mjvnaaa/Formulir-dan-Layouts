ba7a54b07ba7e5b400071b8b4eed977574a992e8
mjvnaaa <dilan271594@gmail.com>
2025-10-01 09:45:30 +0700

    Laporan Praktikum

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ Route::get('/form', [FormController::class, 'create'])->name('form.create');
 Route::post('/form', [FormController::class, 'store'])->name('form.store');
 
 use App\Http\Controllers\MahasiswaController;
-
 Route::resource('mahasiswa', MahasiswaController::class);
 
 
